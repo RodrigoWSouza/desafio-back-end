b40f300f1f70448099f6b40234449f45fba44bab
Rodrigo Souza <rodrigo-w-s@hotmail.com>
2021-04-23 14:47:24 -0300

    definindo rotas e controllers da api

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,20 @@
 <?php
-
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::group(['namespace' => 'Api'], function () {
+    Route::group(['prefix' => 'user'], function () {
+        Route::get('/', 'UserController@index');
+        Route::get('/{id}', 'UserController@show');
+        Route::post('/', 'UserController@store');
+        Route::put('/{id}', 'UserController@update');
+        Route::delete('/{id}', 'UserController@destroy');
+    });
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::group(['prefix' => 'transaction'], function () {
+        Route::get('/', 'TransactionsController@index');
+        Route::get('/{id}', 'TransactionsController@show');
+        Route::post('/', 'TransactionsController@store');
+    });
 });
+
+
