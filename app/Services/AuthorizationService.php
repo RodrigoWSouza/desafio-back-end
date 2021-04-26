e51f8932773345d7075a94ccb15776efa5666034
Rodrigo Souza <rodrigo-w-s@hotmail.com>
2021-04-26 17:43:21 -0300

    services uteis do sistema

---
+++ app/Services/AuthorizationService.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Services;
+
+use App\Exceptions\AuthorizationException;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Http;
+
+class AuthorizationService
+{
+    const SERVICE_URL = 'https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6';
+
+    const AUTHORIZED = 'Autorizado';
+
+    public function approved($transactionId): bool
+    {
+        $response = $this->process($transactionId);
+
+        if (!$response) {
+            throw new AuthorizationException();
+        }
+
+        return true;
+    }
+
+    private function process($transactionId) : bool
+    {
+        $response = $this->request($transactionId);
+
+        $body = $response->json();
+
+        return $this->isApproved($response, $body);
+    }
+
+    private function request($transactionId)
+    {
+        return Http::get(sprintf(self::SERVICE_URL, $transactionId));
+    }
+
+    private function isApproved($response, $body) : bool
+    {
+        return (
+            $response->status() === Response::HTTP_OK
+            && !empty($body['message'])
+            && $body['message'] === self::AUTHORIZED
+        );
+    }
+}
---
+++ app/Services/NotificationService.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Services;
+
+use App\Exceptions\SendNotifyException;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Http;
+
+class NotificationService
+{
+    const SERVICE_URL = 'https://run.mocky.io/v3/b19f7b9f-9cbf-4fc6-ad22-dc30601aec04';
+
+    const SENT = 'Enviado';
+
+    public function send(array $message): bool
+    {
+        $response = $this->process($message);
+
+        if (!$response) {
+            throw new SendNotifyException();
+        }
+
+        return true;
+    }
+
+    private function process(array $message) : bool
+    {
+        $response = $this->request($message);
+
+        $body = $response->json();
+
+        return $this->isSent($response, $body);
+    }
+
+    private function request(array $message)
+    {
+        return Http::post(self::SERVICE_URL, $message);
+    }
+
+    private function isSent($response, $body) : bool
+    {
+        return (
+            $response->status() === Response::HTTP_OK
+            && !empty($body['message'])
+            && $body['message'] === self::SENT
+        );
+    }
+}
