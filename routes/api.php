4587c8b32f6fedfdb321163ff4b6c6dc6ba46026
Rodrigo Souza <rodrigo-w-s@hotmail.com>
2021-04-26 17:45:48 -0300

    rotas api

--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['namespace' => 'Api'], function () {
     Route::group(['prefix' => 'user'], function () {
-        Route::get('/', 'UserController@index');
-        Route::get('/{id}', 'UserController@show');
         Route::post('/', 'UserController@store');
-        Route::put('/{id}', 'UserController@update');
-        Route::delete('/{id}', 'UserController@destroy');
     });
 
     Route::group(['prefix' => 'transaction'], function () {
-        Route::get('/', 'TransactionsController@index');
-        Route::get('/{id}', 'TransactionsController@show');
         Route::post('/', 'TransactionsController@store');
     });
 });
